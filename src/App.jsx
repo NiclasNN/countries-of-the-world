f7f33c49c1b34d9e7671d5990290d637affa9fd2
NiclasNN <niclasnorman1@hotmail.se>
2024-05-18 10:29:36 +0200

    Continer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-
 import './App.css';
-import Navbar from './components/navbar/Navbar.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
+import Container from './components/Container/Container.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,7 +9,13 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* Annat innehåll här */}
+      <Container>
+        {/* Här kan du lägga till land-komponenter eller annan innehåll */}
+        <div className="country-item">Land 1</div>
+        <div className="country-item">Land 2</div>
+        <div className="country-item">Land 3</div>
+        {/* Lägg till fler land-komponenter här */}
+      </Container>
     </>
   );
 }
