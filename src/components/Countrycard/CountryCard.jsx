8299e342d34989319ed82e36a4e39279ffe4d3fe
NiclasNN <niclasnorman1@hotmail.se>
2024-05-23 08:27:34 +0200

    countrycard4.0

--- src/components/Countrycard/CountryCard.jsx
+++ src/components/Countrycard/CountryCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './CountryCard.css';
 
 const CountryCard = ({ country }) => {
   return (
-    <div className="country-card">
-      <img src={country.flags.svg} alt={`Flag of ${country.name.common}`} />
+    <Link to={`/country/${country.cca3}`} className="country-card">
+      <img src={country.flags.svg} alt={`${country.name.common} flag`} />
       <h3>{country.name.common}</h3>
-      <Link to={`/country/${country.cca3}`}>Learn More</Link>
-    </div>
+      <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
+      <p><strong>Region:</strong> {country.region}</p>
+      <p><strong>Capital:</strong> {country.capital ? country.capital[0] : 'N/A'}</p>
+    </Link>
   );
 };
 
